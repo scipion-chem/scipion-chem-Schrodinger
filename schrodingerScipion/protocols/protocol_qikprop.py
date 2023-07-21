9f36126288c972d6b933e79d19eb23c787ca2399
MartinSalinas98 <ssalinasmartin@gmail.com>
2023-07-21 12:34:46 +0200

    Some params

--- schrodingerScipion/protocols/protocol_qikprop.py
+++ schrodingerScipion/protocols/protocol_qikprop.py
@@ -29,30 +29,50 @@ import os
 
 # Scipion em imports
 from pwem.protocols import EMProtocol
+from pyworkflow.protocol.params import PointerParam, BooleanParam
 
 # Plugin imports
 from schrodingerScipion import Plugin
 
 class ProtSchrodingerQikprop(EMProtocol):
-	""" TODO: find out & place here"""
+	""" TODO: find out & place here """
 	_label = 'qikprop'
 	_program = ""
 
 	def _defineParams(self, form):
-		pass
+		""" This function defines the params to be included in the protocol's form. """
+		form.addSection(label='Input')
+		form.addParam('inputSmallMolecules', PointerParam, pointerClass="SetOfSmallMolecules",
+			label='Input small molecules:', help='Input small molecules to be treated.')
+		form.addParam('neut', BooleanParam, default=True, label='Neutralize molecules:',
+			help='Neutralize molecules in Maestro formatted files prior to processing.')
+		form.addParam('sim', BooleanParam, default=True, label='Generate similar known drugs:',
+			help='Generate a list of known drugs most similar to each processed molecule.')
 
 	# --------------------------- INSERT steps functions --------------------
 	def _insertAllSteps(self):
-		""" This function inserts all steps functions that will be run when running the protocol """
+		""" This function inserts all steps functions that will be run when running the protocol. """
 		self._insertFunctionStep('runQikpropStep')
 	
 	def runQikpropStep(self):
-		""" This function runs the schrodinger binary file with the given params """
-		# Getting binary file
-		schrodinger = self.getQikpropBinaryFile()
-		print("BINARY FILE:", schrodinger)
+		""" This function runs the schrodinger binary file with the given params. """
+		print("CMD:", self.getQikpropCmd())
 	
 	# --------------------------- Utils functions --------------------
+	def getQikpropCmd(self):
+		""" This function returns the command string to run qikprop. """
+		# Command starts with the executable file
+		command = self.getQikpropBinaryFile()
+
+		# Add neut flag
+		command += f' {self.getNeutFlag()}'
+
+		# Add sim flag
+		command += f' {self.getSimFlag()}'
+
+		# Return formatted command string
+		return command
+
 	def getQikpropBinaryFile(self):
 		""" This function returns the location for the Schrodinger qikprop binary file. """
 		# Getting path to the binary
@@ -64,3 +84,11 @@ class ProtSchrodingerQikprop(EMProtocol):
 		
 		# If path was not found, raise exception
 		raise FileNotFoundError(f"Path \"{binaryPath}\" not found. Is variable SCHRODINGER_HOME properly set within scipion.conf file?")
+	
+	def getNeutFlag(self):
+		""" This function returns the flag string corresponding to the neut flag param. """
+		return '-neut' if self.neut.get() else '-noneut'
+	
+	def getSimFlag(self):
+		""" This function returns the flag string corresponding to the sim flag param. """
+		return '-sim' if self.sim.get() else '-nosim'
